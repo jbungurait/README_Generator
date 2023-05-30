b65161f252cbe443e71d5e6c25fc1c6910fc0324
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-05-30 11:28:21 -0700

    new setup

--- index.js
+++ index.js
@@ -1,42 +1,80 @@
-
 const fs = require('fs');
+const inquirer = require('inquirer');
+const genMarkdown = require('./utils/generateMarkdown');
 const fileName = 'README.md';
 
 
 // TODO: Create an array of questions for user input
+const  questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe the What, Why, and How:',
+    },
+    {
+        type: 'input',
+        name: 'tableOfContents',
+        message: 'What are the Contents of the README.md file?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your package?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How will the application be used?',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List the credits for the application:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select a license:',
+        choices: [ 
+            'MIT License', 
+            'Eclipse Public License 2.0', 
+            'GNU Affero General Public Livense v3.0', 
+            'GNU General Public License v2.0', 
+            'Mozilla Public License 2.0', 
+            'The Unlicense' ],
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'List the features of your app.',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can other users contribute to the development?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How will the application be tested?',
+        response: '',
+    },
+];
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, userInput) {
 
-//     const Body = `${userInput.Title} \t 
-//     Description: \t ${userInput.Description} \t 
-//     Table of Contents: \t ${userInput.TOC} \t
-//     Installation: \t ${userInput.Install} \t
-//     Usage: \t ${userInput.Usage} \t
-//     Credits: \t ${userInput.Credits} \t
-//     License: \t ${userInput.License} \t
-//     Features: \t ${userInput.Features} \t
-//     Contributions: \t ${userInput.Contribution} \t
-//     Testing: \t ${userInput.Tests}\t`;
-
-//     fs.writeFile(fileName, Body,
-//     (err) => err ? console.error(err) : console.log('Success!'));
-
-// };
-
-
-// TODO: Create a function to initialize app
 function init() {
-        
+    inquirer.prompt(questions).then((response) => {
+        fs.writeFile(fileName, 
+        genMarkdown(response), 
+        (err) => err ? console.log(err) : console.log("File generated!"))
+    })
         
     }
 
 
-
-
-//    const writeFile = new writeToFile();
-//    writeFile(`${fileName}`, userInput);
-
-
-// Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,64 @@
+require('dotenv').config();
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const licenseBadge = '';
+  switch(license) {
+    case 'MIT License':
+      licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+      break;
+    case 'Eclipse Public License 2.0':
+      licenseBadge = `[![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://opensource.org/licenses/EPL-2.0)`
+      break;
+    case 'GNU Affero General Public Livense v3.0':
+      licenseBadge = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`
+      break;
+    case 'GNU General Public License v2.0':
+      licenseBadge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
+      break;
+    case 'Mozilla Public License 2.0':
+      licenseBadge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+      break;
+    case 'The Unlicense':
+      licenseBadge = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+      break;
+    case '':
+      licenseBadge = ""
+      break;
+  }
+return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {
+//   const licenseLink = '';
+//   switch(license) {
+//     case 'MIT License':
+//       licenseLink = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+//       break;
+//     case 'Eclipse Public License 2.0':
+//       licenseLink = `[![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://opensource.org/licenses/EPL-2.0)`
+//       break;
+//     case 'GNU Affero General Public Livense v3.0':
+//       licenseLink = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`
+//       break;
+//     case 'GNU General Public License v2.0':
+//       licenseLink = `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
+//       break;
+//     case 'Mozilla Public License 2.0':
+//       licenseLink = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+//       break;
+//     case 'The Unlicense':
+//       licenseLink = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+//       break;
+//     case '':
+//       licenseLink = ""
+//       break;
+//   }
+// return licenseLink;
+// }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,6 +67,60 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - Installation [#installation]
+
+  - Usage [#usage]
+
+  - Credits [#credits]
+
+  - License [#license]
+
+  - Features [#features]
+
+  - Contribution [#contribution]
+
+  - Tests [#tests]
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Credits
+
+  ${data.credits}
+
+  ### [Github](https://github.com/jbungurait)
+  ### [Email](mailto:${process.env.email})
+  ### [LinkedIn](https://www.linkedin.com/in/josh-ungurait-0045b352/)
+
+  ## License
+
+  This project licensed under ${renderLicenseBadge(data.license)}.
+
+  ## Features
+
+  ${data.features}
+
+  ## Contribution
+
+  ${data.contribution}
+
+  ## Tests 
+
+  ${data.tests}
+
 
 `;
 }
