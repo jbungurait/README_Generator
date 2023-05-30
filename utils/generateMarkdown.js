539a5b8782fc40a1f31373a6b3ca43795741e998
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-05-30 13:00:46 -0700

    still cant get button clicks

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,22 +4,22 @@ function renderLicenseBadge(license) {
   let licenseBadge = '';
   switch(license) {
     case 'MIT License':
-      licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+      licenseBadge = `${license} [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
       break;
     case 'Eclipse Public License 2.0':
-      licenseBadge = `[![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://opensource.org/licenses/EPL-2.0)`
+      licenseBadge = `${license} [![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://opensource.org/licenses/EPL-2.0)`
       break;
     case 'GNU Affero General Public Livense v3.0':
-      licenseBadge = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`
+      licenseBadge = `${license} [![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`
       break;
     case 'GNU General Public License v2.0':
-      licenseBadge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
+      licenseBadge = `${license} [![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
       break;
     case 'Mozilla Public License 2.0':
-      licenseBadge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+      licenseBadge = `${license} [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
       break;
     case 'The Unlicense':
-      licenseBadge = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+      licenseBadge = `${license} [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
       break;
     case '':
       licenseBadge = ""
